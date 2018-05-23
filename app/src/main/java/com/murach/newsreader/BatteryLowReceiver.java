ffc31b73ab71bdfd43a16f49631a08c649face99
Stanislava  Meredith <smeredith@scciti.itp.seattlecentral.edu>
2018-05-22 19:00:09 -0700

    Initial commit

---
+++ app/src/main/java/com/murach/newsreader/BatteryLowReceiver.java
@@ -0,0 +1,4 @@
+package com.murach.newsreader;
+
+public class BatteryLowReceiver {
+}
---
+++ app/src/main/java/com/murach/newsreader/ConnectivityReceiver.java
@@ -0,0 +1,11 @@
+package com.murach.newsreader;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+public class ConnectivityReceiver {
+}
